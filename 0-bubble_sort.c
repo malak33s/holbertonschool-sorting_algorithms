cd62566255f528ae6df06f8e8f7df5e6e5b7784f
AxelNAY <axelnay@sfr.fr>
2024-04-11 13:52:41 +0000

    Update the file 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,12 +9,12 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0;
+	size_t i = 0, j;
 	int bub;
 
 	for (i = 0; i < 15; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (a[j] > a[j++])
 			{
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 				a[j++] = bub;
 			}
 			printf("%d", a[j]);
-			if (j < size - 2)
+			if (j < size - 1)
 				printf(", ");
 		}
 		printf("\n");
