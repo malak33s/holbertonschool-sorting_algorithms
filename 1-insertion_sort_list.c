ebef2d3083363db1fd5f80d07e7084567633b660
AxelNAY <axelnay@sfr.fr>
2024-04-14 20:35:37 +0000

    Update the file 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,9 +11,8 @@ void insertion_sort_list(listint_t **list)
 	listint_t *head, *temp, *prev;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-	{
 		return;
-	}
+
 	head = (*list)->next;
 
 	while (head != NULL)
